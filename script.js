aedcd470754b16dd542af3777e70e84153570810
kristijanH1998 <kristijan.hornung1@gmail.com>
2024-02-12 04:11:17 -0800

    spaceship movement and rotation implemented, initial projectile code

--- script.js
+++ script.js
@@ -10,6 +10,7 @@ class Player {
             x: 0,
             y: 0
         };
+        this.rotation = 0;
         const image = new Image();
         image.src = './spaceship.png';
         image.onload = () => {
@@ -26,19 +27,118 @@ class Player {
     draw(){
      //   c.fillStyle = 'red',
       //  c.fillRect(this.position.x, this.position.y, this.width, this.height)
+        c.save();
+        c.translate(player.position.x + player.width / 2, 
+                    player.position.y + player.height / 2);
+        c.rotate(this.rotation);
+        c.translate(-player.position.x - player.width / 2, 
+                    -player.position.y - player.height / 2);
         if(this.image){
-            c.drawImage(this.image, this.position.x, this.position.y, this.width, this.height);
+            c.drawImage(this.image, 
+                        this.position.x, 
+                        this.position.y, 
+                        this.width, 
+                        this.height);
         }
+        c.restore();
+    }
+
+    update() {
+        if(this.image) {
+            this.draw();
+            this.position.x += this.velocity.x;
+        }
+    
+    }
+}
+
+class Projectile {
+    constructor({position, velocity}) {
+        this.position = position;
+        this.velocity = velocity;
+        this.radius = 3;
+    }
+    draw() {
+        c.beginPath();
+        c.arc(this.position.x, this.position.y, this.radius, 0, Math.PI * 2);
+        c.fillStyle = 'red';
+        c.fill();
+        c.closePath();
+    }
+    update(){
+        this.draw();
+        this.position.x += this.velocity.x;
+        this.position.y += this.velocity.y;
     }
 }
 
 const player = new Player();
-player.draw();
+const projectiles = [];
+const keys = {
+    a: {
+        pressed: false
+    },
+    d: {
+        pressed: false
+    },
+    space: {
+        pressed: false
+    }
+}
 
 function animate() {
     requestAnimationFrame(animate);
     c.fillStyle = 'black';
     c.fillRect(0, 0, canvas.width, canvas.height);
-    player.draw();
+    player.update();
+    projectiles.forEach(projectile => {
+        projectile.update();
+    });
+    if (keys.a.pressed && player.position.x >= 0){
+        player.velocity.x = -5;
+        player.rotation = -.15;
+    } else if (keys.d.pressed && (player.position.x + player.width <= canvas.width)) {
+        player.velocity.x = 5;
+        player.rotation = .15;
+    } else {
+        player.velocity.x = 0;
+        player.rotation = 0;
+    }
 }
 animate();
+
+addEventListener('keydown', ({ key }) => {
+    switch(key){
+        case 'a': 
+            keys.a.pressed = true;
+            break;
+        case 'd': 
+            keys.d.pressed = true;
+            break; 
+        case ' ':
+            projectiles.push(new Projectile({
+                position: {
+                    x: 300,
+                    y: 300
+                },
+                velocity: {
+                    x: 5,
+                    y: 5
+                }
+            })); 
+            break;
+    }
+});
+addEventListener('keyup', ({ key }) => {
+    switch(key){
+        case 'a': 
+            keys.a.pressed = false;
+            break;
+        case 'd': 
+            keys.d.pressed = false;
+            break; 
+        case ' ': 
+            keys.space.pressed = false;
+            break;
+    }
+});
